f5920fdcf6194227d98005bbfe896b693fcb14ed
Daniel Aguirre <uhyeay24@gmail.com>
2021-09-02 11:50:46 -0500

    exercise completed

--- ConsoleUI/Program.cs
+++ ConsoleUI/Program.cs
@@ -24,6 +24,8 @@ namespace ConsoleUI
              * Vehicle shall have a virtual method called DriveVirtual with a base implementation.
              */
 
+            // Done
+
             /* 
              * Now create 2 non-abstract classes: Car and Motorcycle, that inherit from Vehicle
              * Add a distict property in the 2 derived classes such as HasTrunk for Car and HasSideCart for Motorcycle
@@ -31,7 +33,11 @@ namespace ConsoleUI
              * Only in the Motorcycle class will you override the virtual drive method
             */
 
+            // Done
+
             // Create a list of Vehicle called vehicles
+
+            var vehicles = new List<Vehicle>();
 
             /*
              * Create 4 instances: 1 Car, 1 Motorcycle, and then 2 instances of type Vehicle (use explicit typing) but use constuctors from derived classes
@@ -39,12 +45,68 @@ namespace ConsoleUI
              * Set the properties with object initializer syntax
              */
 
+            vehicles.Add(new Car()
+            {
+                Year = 1994,
+                HasTrunk = false, 
+                Make = "Car",
+                Model = "type Car"
+            });
+
+            vehicles.Add(new Motorcycle()
+            {
+                Year = 1995,
+                HasSideCart = false,
+                Make = "Motorcycle",
+                Model = "type Motorcycle"
+            });
+
+            Vehicle carVehicle = new Car()
+            {
+                Year = 1996,
+                HasTrunk = true,
+                Make = "Car",
+                Model = "type Vehicle"
+            };
+
+            Vehicle motorcycleVehicle = new Motorcycle()
+            {
+                Year = 1997,
+                HasSideCart = true,
+                Make = "Motorcycle",
+                Model = "type vehicle"
+            };
+
+            vehicles.Add(carVehicle);
+            vehicles.Add(motorcycleVehicle);
+
             /*
              * Add the 4 vehicles to the list
              * Using a foreach loop iterate over each of the properties
              */
 
+            foreach (var vehicle in vehicles)
+            {
+                Console.WriteLine("\n***************************");
+                Console.WriteLine($"Vehicle at index {vehicles.IndexOf(vehicle)}: ");
+                Console.WriteLine($"Year: {vehicle.Year}\nMake: {vehicle.Make}\nModel: {vehicle.Model}");
+                Console.WriteLine();
+            }
+
+            Console.ReadLine();
             // Call each of the drive methods for one car and one motorcycle
+
+            foreach (var vehicle in vehicles)
+            {
+                Console.WriteLine("\n***************************");
+                Console.WriteLine($"Vehicle at index {vehicles.IndexOf(vehicle)}: ");
+                Console.WriteLine("DriveAbstract: ");
+                vehicle.DriveAbstract();
+                Console.WriteLine("DriveVirtual:");
+                vehicle.DriveVirtual();
+                Console.WriteLine();
+
+            }
 
             #endregion            
             Console.ReadLine();
---
+++ ConsoleUI/Vehicle.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleUI
+{
+    abstract class Vehicle
+    {
+        /*
+         * Create an abstract class called Vehicle
+         * The vehicle class shall have three string properties Year, Make, and Model
+         * Set the defaults to something generic in the Vehicle class
+         * Vehicle shall have an abstract method called DriveAbstract with no implementation
+         * Vehicle shall have a virtual method called DriveVirtual with a base implementation.
+         */
+
+        public int Year { get; set; } = 0;
+        public string Make { get; set; } = "Unknown";
+        public string Model { get; set; } = "Unknown";
+
+        public abstract void DriveAbstract();
+
+        public virtual void DriveVirtual()
+        {
+            Console.WriteLine($"You are driving a {GetType().Name}");
+        }
+
+
+    }
+}
